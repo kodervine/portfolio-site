c30a3d73953cf1094763950f890bea50cc57e303
Laskellyb <anikwenzekelly@gmail.com>
2023-02-20 15:08:19 +0100

    implemented the Browser routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
