b4d1002e26bf817c308d9f239f244825b7709203
Laskellyb <anikwenzekelly@gmail.com>
2023-02-21 01:15:01 +0100

    successfully imported spectral font

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppProvider } from "./contexts/AppContext";
 import App from "./App";
+import "./App.css";
 import { Badge, Button, MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
