347c428ae9467c444cf4de723016b0cd8a1b1c6b
Laskellyb <anikwenzekelly@gmail.com>
2023-02-21 01:26:52 +0100

    overflow not working

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,13 +1,29 @@
 import React from "react";
-import { Navbar, Image, Box, Text, Anchor, Flex } from "@mantine/core";
+import {
+  Navbar,
+  Image,
+  Box,
+  Text,
+  Anchor,
+  Flex,
+  createStyles,
+} from "@mantine/core";
 import profileImg from "../assets/profile-pic.jpg";
 import { nanoid } from "nanoid";
 import { ListItem, profileListItems } from "../data";
 import Footer from "./Footer";
 
+const useStyles = createStyles((theme) => ({
+  nav: {
+    height: "100vh",
+    overflow: "hidden",
+  },
+}));
 const NavItems: React.FC = () => {
+  const { classes } = useStyles();
+
   return (
-    <Navbar display="flex">
+    <Navbar display="flex" className={classes.nav}>
       <Flex>
         <Image src={profileImg} radius="lg" width="50px" height="50px" />
       </Flex>
