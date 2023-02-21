de2102dd574c1480b52fb8de7443cc056b553763
Laskellyb <anikwenzekelly@gmail.com>
2023-02-21 09:48:25 +0100

    Accordion page relatively done for now

--- src/data.tsx
+++ src/data.tsx
@@ -6,7 +6,6 @@ import {
   AiOutlineTwitter,
   AiOutlineMedium,
 } from "react-icons/ai";
-import { FaGithubSquare } from "react-icons/fa";
 import { SiHashnode } from "react-icons/si";
 
 export interface ListItem {
@@ -58,18 +57,48 @@ export const iconData: IconItem[] = [
 
 export interface PortfolioInterface {
   title: string;
-  description1: string;
-  descriptionItems: string;
-  moreDetailsLink: string;
+  description: string;
+  link: string;
   technologiesUsed: string[];
+  githubIcon: React.ReactElement;
+  githubLink: string;
 }
 
 export const portfolioData: PortfolioInterface[] = [
   {
-    title: "Ceendit",
-    description1: "",
-    descriptionItems: "",
-    moreDetailsLink: "",
-    technologiesUsed: ["React", "Chakra UI"],
+    title: "Ceendit -  an invoice generator.",
+    description:
+      "Say goodbye to manual invoicing and hello to effortless payment processing. Create professional invoices in pdf format for your clients in less than a minute",
+    link: "",
+    githubIcon: <AiFillGithub />,
+    githubLink: "https://github.com/kodervine/ceendit-react",
+    technologiesUsed: ["React", "Javascript", "Chakra UI", "Firebase"],
+  },
+  {
+    title: "Pryto - live crypto currency converter",
+    description:
+      "This is a live crypto currency converter to naira. Created a multi-page app that updates select crypto currency price in real time with css3, Tailwind css, and vanilla Javascript. The data is powered by the Coingecko APIs to gain access to the current crypto currencies market values.",
+    link: "https://kodervine.github.io/pryto/",
+    githubIcon: <AiFillGithub />,
+    githubLink: "https://github.com/kodervine/pryto",
+    technologiesUsed: ["Javascript", "Tailwind", "Coingecko API"],
+  },
+  {
+    title: "Kadd - customizable business card creator",
+    description:
+      "This is a customizable business card creator. Created with HTML, CSS, and Javascript.  Create a customizable business card in just a few clicks. Can change to the theme color of your choice, and save to a png file in seconds",
+    link: "https://kadd.netlify.app/",
+    githubIcon: <AiFillGithub />,
+    githubLink: "https://github.com/kodervine/kadd-business-card-creator",
+    technologiesUsed: ["Javascript", "CSS", "HTML"],
+  },
+  {
+    title: "My Tasks - daily schedule tools",
+    description:
+      "This is a simple daily schedule programme. Created with HTML, Tailwind CSS, and Javascript.Have an organised day by creating simple daily-task reminders. Made with User friendly interface and easy navigation in mind",
+    link: "https://my-dailytask.netlify.app/",
+    githubIcon: <AiFillGithub />,
+    githubLink: "https://github.com/kodervine/my-daily-task",
+    technologiesUsed: ["Javascript", "HTML", "Bootstrap"],
   },
 ];
--- src/pages/PortfolioPage.tsx
+++ src/pages/PortfolioPage.tsx
@@ -1,9 +1,59 @@
 import { FC } from "react";
 import { portfolioData, PortfolioInterface } from "../data";
-import { Accordion } from "@mantine/core";
+import {
+  Accordion,
+  Anchor,
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  createStyles,
+  Text,
+} from "@mantine/core";
 import { AiOutlinePlus } from "react-icons/ai";
 
+const useStyles = createStyles((theme) => ({
+  root: {
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[6]
+        : theme.colors.gray[0],
+    borderRadius: theme.radius.sm,
+  },
+
+  item: {
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[6]
+        : theme.colors.gray[0],
+    border: "1px solid transparent",
+    position: "relative",
+    zIndex: 0,
+    transition: "transform 150ms ease",
+
+    "&[data-active]": {
+      transform: "scale(1.02)",
+      backgroundColor:
+        theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+      boxShadow: theme.shadows.md,
+      borderColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[4]
+          : theme.colors.gray[2],
+      borderRadius: theme.radius.md,
+      zIndex: 1,
+    },
+  },
+
+  chevron: {
+    "&[data-rotate]": {
+      transform: "rotate(-90deg)",
+    },
+  },
+}));
+
 const PortfolioPage: FC = () => {
+  const { classes } = useStyles();
   return (
     <div>
       <h1>Portfolio</h1>
@@ -17,38 +67,45 @@ const PortfolioPage: FC = () => {
             },
           },
         }}
+        mx="10px"
+        variant="filled"
+        classNames={classes}
+        className={classes.root}
       >
         {portfolioData.map((item: PortfolioInterface) => (
           <Accordion.Item value={item.title} key={item.title}>
             <Accordion.Control>{item.title}</Accordion.Control>
             <Accordion.Panel>
-              {item.description1}
-              <ul>
-                {item.descriptionItems.split(",").map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              </ul>
-              <a href={item.moreDetailsLink}>More details</a>
-              <p>Technologies used: {item.technologiesUsed.join(", ")}</p>
+              <Text size="14px">{item.description}</Text>
+
+              <Box>
+                <Anchor href={item.link} target="_blank">
+                  More details
+                </Anchor>
+              </Box>
+
+              <Box>
+                <Text weight="bold" size="14px">
+                  Technologies used:
+                </Text>
+
+                {item.technologiesUsed.map((btn, index) => {
+                  return (
+                    <Badge
+                      color="orange"
+                      size="sm"
+                      variant="dot"
+                      key={index}
+                      mr="10px"
+                    >
+                      {btn}
+                    </Badge>
+                  );
+                })}
+              </Box>
             </Accordion.Panel>
           </Accordion.Item>
         ))}
-
-        <Accordion.Item value="flexibility">
-          <Accordion.Control>Flexibility</Accordion.Control>
-          <Accordion.Panel>
-            Configure components appearance and behavior with vast amount of
-            settings or overwrite any part of component styles
-          </Accordion.Panel>
-        </Accordion.Item>
-
-        <Accordion.Item value="focus-ring">
-          <Accordion.Control>No annoying focus ring</Accordion.Control>
-          <Accordion.Panel>
-            With new :focus-visible pseudo-class focus ring appears only when
-            user navigates with keyboard
-          </Accordion.Panel>
-        </Accordion.Item>
       </Accordion>
     </div>
   );
