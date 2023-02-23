37e338e55ac91c82c04adf00628d537273491f5c
Laskellyb <anikwenzekelly@gmail.com>
2023-02-23 22:59:12 +0100

    still deploying the site

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { AppProvider } from "./contexts/AppContext";
 import App from "./App";
 import "./App.css";
-import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
