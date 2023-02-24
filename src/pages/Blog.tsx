3e98f2668fbe5a5159c03ebf311bb79551f01a6d
Laskellyb <anikwenzekelly@gmail.com>
2023-02-24 14:59:03 +0100

    Fix: conditional render blog page only when data has been fetched

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -35,61 +35,75 @@ const Blog = (props: Props) => {
       <Title order={3} mt="md">
         Published blogs
       </Title>
-      <Grid align="center" justify="center">
-        {blogData.map((blogs: any, index: number) => {
-          const { title, brief, slug, coverImage, dateAdded } = blogs;
-          return (
-            <Grid.Col md={6} lg={3.5} ref={ref} key={nanoid()}>
-              <Card
-                shadow={hovered ? "xl" : "md"}
-                p="xl"
-                px={20}
-                component="a"
-                key={nanoid()}
-                my={10}
-                mx={{ sm: 20, md: 10 }}
-                radius="lg"
-              >
-                <Card.Section>
-                  <Image
-                    src={coverImage}
-                    height={200}
-                    alt="course certification images"
-                  />
-                </Card.Section>
 
-                <Flex direction="column" align="start">
-                  <Text weight={500} size="lg" my="md">
-                    {title}
-                  </Text>
-                  <Badge color="yellow" size="sm" variant="dot" mr="10px">
-                    {dateAdded}
-                  </Badge>
+      {blogData.length === 0 ? (
+        <Text size={20}>Loading...</Text>
+      ) : (
+        <Grid align="center" justify="center">
+          {blogData.map((blogs: any, index: number) => {
+            const { title, brief, slug, coverImage, dateAdded } = blogs;
+            return (
+              <Grid.Col md={6} lg={3.5} ref={ref} key={nanoid()}>
+                <Card
+                  shadow={hovered ? "xl" : "md"}
+                  p="xl"
+                  px={20}
+                  component="a"
+                  key={nanoid()}
+                  my={10}
+                  mx={{ sm: 20, md: 10 }}
+                  radius="lg"
+                >
+                  <Card.Section>
+                    <Image
+                      src={coverImage}
+                      height={200}
+                      alt="course certification images"
+                    />
+                  </Card.Section>
 
-                  <Text mt="xs" color="dimmed" size="sm">
-                    {brief}{" "}
-                    <a
-                      href={`https://kodervine.hashnode.dev/${slug}`}
-                      target="_blank"
-                    >
-                      Read more
-                    </a>
-                  </Text>
-                </Flex>
-              </Card>
-            </Grid.Col>
-          );
-        })}
-      </Grid>
-      <Button>
-        <Anchor
-          href="https://kodervine.hashnode.dev"
-          target="_blank"
-          style={{ color: "white", textDecoration: "none" }}
-        >
-          Read more articles
-        </Anchor>
-      </Button>
+                  <Flex direction="column" align="start">
+                    <Text weight={500} size="lg" my="md">
+                      {title}
+                    </Text>
+                    <Badge color="yellow" size="sm" variant="dot" mr="10px">
+                      {dateAdded}
+                    </Badge>
+
+                    <Text mt="xs" color="dimmed" size="sm">
+                      {brief}{" "}
+                      <a
+                        href={`https://kodervine.hashnode.dev/${slug}`}
+                        target="_blank"
+                      >
+                        Read more
+                      </a>
+                    </Text>
+                  </Flex>
+                </Card>
+              </Grid.Col>
+            );
+          })}
+        </Grid>
+      )}
+
+      <Box
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "10px",
+        }}
+      >
+        <Button>
+          <Anchor
+            href="https://kodervine.hashnode.dev"
+            target="_blank"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            Read more articles
+          </Anchor>
+        </Button>
+      </Box>
     </Box>
   );
 };
